GNOME/NetworkManager-openvpn

commit 627453c2dec8d5eb4fbc775b3d9afa7c25491350

core: use '--auth-retry interact' to query for failed passwords
If authentication fails allow the management interface to ask for
new passwords instead of just terminating the VPN connection.

src/nm-openvpn-service.c
@@ -546,17 +546,30 @@ handle_management_socket (NMVPNPlugin *plugin,
 
 	auth = get_detail (str, ">PASSWORD:Verification Failed: '");
 	if (auth) {
-		if (!strcmp (auth, "Auth"))
+		gboolean fail = TRUE;
+
+		if (!strcmp (auth, "Auth")) {
 			g_warning ("Password verification failed");
-		else if (!strcmp (auth, "Private Key"))
+			if (priv->interactive) {
+				/* Clear existing password in interactive mode, openvpn
+				 * will request a new one after restarting.
+				 */
+				if (priv->io_data->password)
+					memset (priv->io_data->password, 0, strlen (priv->io_data->password));
+				g_clear_pointer (&priv->io_data->password, g_free);
+				fail = FALSE;
+			}
+		} else if (!strcmp (auth, "Private Key"))
 			g_warning ("Private key verification failed");
 		else
 			g_warning ("Unknown verification failed: %s", auth);
 
-		g_free (auth);
+		if (fail) {
+			*out_failure = NM_VPN_PLUGIN_FAILURE_LOGIN_FAILED;
+			again = FALSE;
+		}
 
-		*out_failure = NM_VPN_PLUGIN_FAILURE_LOGIN_FAILED;
-		again = FALSE;
+		g_free (auth);
 	}
 
 out:
@@ -1184,6 +1197,8 @@ nm_openvpn_start_openvpn_binary (NMOpenvpnPlugin *plugin,
 
 	/* Query on the management socket for user/pass */
 	add_openvpn_arg (args, "--management-query-passwords");
+	add_openvpn_arg (args, "--auth-retry");
+	add_openvpn_arg (args, "interact");
 
 	/* do not let openvpn setup routes or addresses, NM will handle it */
 	add_openvpn_arg (args, "--route-noexec");

commit 5afb8eb0128d695376d6165a47e313dae5157f0d

service: fix compilation with GLib 2.32
g_clear_pointer() is only available from GLib 2.34 and higher.

Regression was introduced in 627453c2dec8d5eb4fbc775b3d9afa7c25491350.

common/nm-glib-compat.h
@@ -1,0 +1,49 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: t; c-basic-offset: 4 -*- */
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Copyright 2014 Red Hat, Inc.
+ */
+
+#ifndef __NM_GLIB_COMPAT_H__
+#define __NM_GLIB_COMPAT_H__
+
+#include <glib.h>
+#include <glib-object.h>
+
+
+#if !GLIB_CHECK_VERSION(2,34,0)
+
+#define g_clear_pointer(pp, destroy)	  \
+	G_STMT_START { \
+		G_STATIC_ASSERT (sizeof *(pp) == sizeof (gpointer)); \
+		/* Only one access, please */ \
+		gpointer *_pp = (gpointer *) (pp); \
+		gpointer _p; \
+		/* This assignment is needed to avoid a gcc warning */ \
+		GDestroyNotify _destroy = (GDestroyNotify) (destroy); \
+	  \
+		(void) (0 ? (gpointer) *(pp) : 0); \
+		do \
+			_p = g_atomic_pointer_get (_pp); \
+		while G_UNLIKELY (!g_atomic_pointer_compare_and_exchange (_pp, _p, NULL)); \
+	  \
+		if (_p) \
+			_destroy (_p); \
+	} G_STMT_END
+
+#endif
+
+#endif  /* __NM_GLIB_COMPAT_H__ */

src/nm-openvpn-service.c
@@ -55,6 +55,7 @@
 #include "nm-openvpn-service.h"
 #include "nm-utils.h"
 #include "common/utils.h"
+#include "common/nm-glib-compat.h"
 
 #if !defined(DIST_VERSION)
 # define DIST_VERSION VERSION