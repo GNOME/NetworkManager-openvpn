GNOME/NetworkManager-openvpn

commit 81149fd01897166cee5649d2da3801f2a5a45b5c

core: use a default renegotiation interval of zero (rh #969433)
Since the client and server do not negotiate options, each side gets
to specify its own --reneg-sec to control when each side renegotiates.
OpenVPN defaults to 3600, so if the client and server don't agree this
causes too-frequent renegotiations.

This is worse with two-factor authentication, becuase it can mean that
the client requests a password/PIN from the user much more often then
the server actually wants.

https://bugzilla.redhat.com/show_bug.cgi?id=969433

src/nm-openvpn-service.c
@@ -1115,6 +1115,14 @@ nm_openvpn_start_openvpn_binary (NMOpenvpnPlugin *plugin,
 			free_openvpn_args (args);
 			return FALSE;
 		}
+	} else {
+		/* Either the server and client must agree on the renegotiation
+		 * interval, or it should be disabled on one side to prevent
+		 * too-frequent renegotiations, which make two-factor auth quite
+		 * painful.
+		 */
+		add_openvpn_arg (args, "--reneg-sec");
+		add_openvpn_arg (args, "0");
 	}
 
 	if (debug) {

commit 7f9031dbaee3059ea072fb2497563aee6acf8da0

service: only set reneg-sec option in TLS mode
Fixes: 81149fd01897166cee5649d2da3801f2a5a45b5c

https://bugzilla.gnome.org/show_bug.cgi?id=749050
https://bugzilla.redhat.com/show_bug.cgi?id=1225218

src/nm-openvpn-service.c
@@ -732,6 +732,14 @@ validate_connection_type (const char *ctype)
 	return NULL;
 }
 
+static gboolean
+connection_type_is_tls_mode (const char *connection_type)
+{
+	return strcmp (connection_type, NM_OPENVPN_CONTYPE_TLS) == 0
+	    || strcmp (connection_type, NM_OPENVPN_CONTYPE_PASSWORD) == 0
+	    || strcmp (connection_type, NM_OPENVPN_CONTYPE_PASSWORD_TLS) == 0;
+}
+
 static const char *
 nm_find_openvpn (void)
 {
@@ -1104,7 +1112,10 @@ nm_openvpn_start_openvpn_binary (NMOpenvpnPlugin *plugin,
 
 	/* Reneg seconds */
 	tmp = nm_setting_vpn_get_data_item (s_vpn, NM_OPENVPN_KEY_RENEG_SECONDS);
-	if (tmp && strlen (tmp)) {
+	if (!connection_type_is_tls_mode (connection_type)) {
+		/* Ignore --reneg-sec option if we are not in TLS mode (as enabled
+		 * by --client below). openvpn will error out otherwise, see bgo#749050. */
+	} else if (tmp && strlen (tmp)) {
 		add_openvpn_arg (args, "--reneg-sec");
 		if (!add_openvpn_arg_int (args, tmp)) {
 			g_set_error (error,