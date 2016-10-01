GNOME/NetworkManager-openvpn

commit 5f592d6aa5aa481eb65be4e630c00f7908316573

import/export: don't limit reneg-sec to 65536 on import
Fixes: cd2fe46ec387fe6ce784f343be5a0dd22f7402af

properties/import-export.c
@@ -932,7 +932,7 @@ do_import (const char *path, const char *contents, gsize contents_len, GError **
 		if (NM_IN_STRSET (params[0], NMV_OVPN_TAG_RENEG_SEC)) {
 			if (!args_params_check_nargs_n (params, 1, &line_error))
 				goto handle_line_error;
-			if (!args_params_parse_int64 (params, 1, 0, 0xffff, &v_int64, &line_error))
+			if (!args_params_parse_int64 (params, 1, 0, 604800, &v_int64, &line_error))
 				goto handle_line_error;
 			setting_vpn_add_data_item_int64 (s_vpn, NM_OPENVPN_KEY_RENEG_SECONDS, v_int64);
 			continue;

commit 5f115c5af505cbe67845459249b52da5b2276c65

import: allow large reneg-sec time values
https://bugzilla.redhat.com/show_bug.cgi?id=1380904

Fixes: 5f592d6aa5aa481eb65be4e630c00f7908316573

properties/import-export.c
@@ -981,7 +981,7 @@ do_import (const char *path, const char *contents, gsize contents_len, GError **
 		if (NM_IN_STRSET (params[0], NMV_OVPN_TAG_RENEG_SEC)) {
 			if (!args_params_check_nargs_n (params, 1, &line_error))
 				goto handle_line_error;
-			if (!args_params_parse_int64 (params, 1, 0, 604800, &v_int64, &line_error))
+			if (!args_params_parse_int64 (params, 1, 0, G_MAXINT, &v_int64, &line_error))
 				goto handle_line_error;
 			setting_vpn_add_data_item_int64 (s_vpn, NM_OPENVPN_KEY_RENEG_SECONDS, v_int64);
 			continue;